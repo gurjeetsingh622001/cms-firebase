ae3aa9cb2f1abfd697c5e2f9e73bb3c144009f52
Gurjeet Singh <114226099+gurjeetsingh622001@users.noreply.github.com>
2023-07-27 10:19:58 +0530

    firebase hosting done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA9Sw-OuLbG1Xs8yBVham_7quMFLugMgsU",
+    authDomain: "customer-management-syst-5b220.firebaseapp.com",
+    databaseURL: "https://customer-management-syst-5b220-default-rtdb.firebaseio.com",
+    projectId: "customer-management-syst-5b220",
+    storageBucket: "customer-management-syst-5b220.appspot.com",
+    messagingSenderId: "391713370108",
+    appId: "1:391713370108:web:94b4c384b990c54bc84c79"
+  },
 };
